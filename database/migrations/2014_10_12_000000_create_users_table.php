560c5970c534a30279ba4fda0763168ccff9bced
Bui The Hanh <s2.ak33@gmail.com>
2019-01-16 14:22:11 +0700

    Create migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,17 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('login_name')->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->string('full_name');
+            $table->string('avatar')->nullable();
+            $table->string('role')->nullable();
+            $table->boolean('is_banned')->default(0);
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
