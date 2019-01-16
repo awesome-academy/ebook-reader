78b29e1379314c829f30b9703b9f1c744cb46545
Bui The Hanh <buihanh2304@gmail.com>
2019-01-16 09:15:53 +0700

    create models

---
+++ app/Models/Meta.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Meta extends Model
+{
+    protected $guarded = ['id'];
+
+    public $timestamps = false;
+
+    public function stories()
+    {
+        return $this->belongsToMany('App\Models\Story', 'meta_story', 'story_id', 'meta_id');
+    }
+}
---
+++ app/Models/User.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    use Notifiable, SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected $guarded = ['id'];
+
+    protected $dates = [
+        'email_verified_at',
+        'created_at',
+        'updated_at',
+        'deteted_at',
+    ];
+
+    protected $casts = [
+        'is_banned' => 'boolean',
+    ];
+
+    public function comments()
+    {
+        return $this->hasMany('App\Models\Comment', 'user_id');
+    }
+
+    public function followers()
+    {
+        return $this->belongsToMany('App\Models\User', 'follows', 'following_user_id', 'followed_user_id');
+    }
+    
+    public function followings()
+    {
+        return $this->belongsToMany('App\Models\User', 'follows', 'followed_user_id', 'following_user_id');
+    }
+
+    public function lists()
+    {
+        return $this->hasMany('App\Models\List', 'user_id');
+    }
+
+    public function notifications()
+    {
+        return $this->hasMany('App\Models\Notifications', 'user_id');
+    }
+
+    public function profile()
+    {
+        return $this->hasOne('App\Models\Profile', 'user_id');
+    }
+
+    public function profilePosts()
+    {
+        return $this->morphMany('App\Models\Comment', 'commentable')->where('parent_id', 0);
+    }
+
+    public function reportings()
+    {
+        return $this->hasMany('App\Models\Report', 'user_id');
+    }
+
+    public function reviewings()
+    {
+        return $this->hasMany('App\Models\Review', 'user_id');
+    }
+
+    public function archives()
+    {
+        return $this->belongsToMany('App\Models\Story', 'archives', 'story_id', 'user_id');
+    }
+
+    public function votings()
+    {
+        return $this->hasMany('App\Models\Vote', 'user_id');
+    }
+}
