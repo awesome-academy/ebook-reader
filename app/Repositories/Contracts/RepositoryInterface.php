1f7d0221d075d88df72f9d8deda183557f2ca930
Bui The Hanh <buihanh2304@gmail.com>
2019-02-21 10:59:50 +0700

    Rewrite repositories

---
+++ app/Repositories/Contracts/RepositoryInterface.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Repositories\Contracts;
+
+interface RepositoryInterface
+{
+    public function where($conditions, $operator = null, $value = null);
+
+    public function orWhere($conditions, $operator = null, $value = null);
+
+    public function orderBy($column, $direction = 'asc');
+
+    public function limit($limit);
+
+    public function offset($offset);
+
+    public function count();
+
+    public function withCount($relations);
+
+    public function select();
+
+    public function all();
+    
+    public function get();
+
+    public function paginate($perPage = null);
+
+    public function find($id);
+
+    public function findOrFail($id);
+
+    public function create(array $data);
+
+    public function update(array $data);
+
+    public function delete($id);
+
+    public function truncate();
+}
